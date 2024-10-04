02966f137acffdfe170c556c11bc3057c0ce8ccb
BuildTools <unconfigured@null.spigotmc.org>
2024-10-03 22:44:46 -0300

    Implementacion base de datos

--- Ecopuntos/Ecopuntos/Ecopuntos/urls.py
+++ Ecopuntos/Ecopuntos/Ecopuntos/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from ecopuntosapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("ecopuntosapp.urls")),
     path("", views.index)
     ]
--- Ecopuntos/Ecopuntos/ecopuntosapp/models.py
+++ Ecopuntos/Ecopuntos/ecopuntosapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Accounts(models.Model):
+    username = models.CharField(max_length= 15)
+    password = models.CharField(max_length= 15)
+    trust = models.IntegerField()
