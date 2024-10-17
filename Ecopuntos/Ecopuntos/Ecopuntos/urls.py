d975fa028c6a2daceb0345d50003f5477118a113
Enerveshub <ajaraa@usm.cl>
2024-10-17 18:37:35 -0300

    cambio a urls en base a la correcion de la ayudantia

--- Ecopuntos/Ecopuntos/Ecopuntos/urls.py
+++ Ecopuntos/Ecopuntos/Ecopuntos/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from ecopuntosapp import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.index)
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls)
     ]
