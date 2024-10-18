6977e9b15d2f3b460bdc1fdda306bde98d267f4b
Enerveshub <ajaraa@usm.cl>
2024-10-18 15:44:18 -0300

    sigue dando error lo de la ayudantia

--- Ecopuntos/Ecopuntos/Ecopuntos/urls.py
+++ Ecopuntos/Ecopuntos/Ecopuntos/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from ecopuntosapp import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path("", views.index)
     ]
+
