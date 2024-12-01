6a89602485af8cd1f7cc3c22ea85f0797aa4ea20
Lagartopichu <jpenao@usm.cl>
2024-12-01 19:18:48 -0300

    se robaron la pala

--- ecopuntosapp/urls.py
+++ ecopuntosapp/urls.py
@@ -3,7 +3,6 @@ from .views import inicio,home,faq,search,forums,ourselves
 
 urlpatterns = [
     path ("", home, name="home"),
-    path ("home/", home, name="home"),
     path ("login/", inicio, name="login"),
     path ("FAQ/", faq, name="faq"),
     path ("search/", search, name="search"),
