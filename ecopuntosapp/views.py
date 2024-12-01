9fb243929d9eb57ca4aa48dce2f9f3d47223e7b8
Lagartopichu <jpenao@usm.cl>
2024-12-01 03:02:06 -0300

    en vola me pitie todo pero es pa organizar

---
+++ ecopuntosapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "ecopuntosapp/ecopuntos.html")
+
+def inicio(request):
+    return render(request, "ecopuntosapp/inicio.html")
+
+def faq(request):
+    return render(request, "ecopuntosapp/FAQ.html")
+
+def search(request):
+    return render(request, "ecopuntosapp/search.html")
+
+def ourselves(request):
+    return render(request, "ecopuntosapp/wha.html")
+
+def forums(request):
+    return render(request, "ecopuntosapp/forums.html")
+
+
+
+# Create your views here.
